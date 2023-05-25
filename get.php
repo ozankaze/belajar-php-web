6d7de4aee0a682a871d96531b7a5265414e08b47
ozankaze <ozankaze101@gmail.com>
2023-05-25 21:28:17 +0700

    XSS (Cross-site scripting)

--- get.php
+++ get.php
@@ -1,6 +1,7 @@
 <?php
 
-$say = "Hello " . $_GET['name'];
+// $say = "Hello " . $_GET['name'];
+$say = "Hello " . htmlspecialchars($_GET['name']);
 
 ?>
 
@@ -12,3 +13,9 @@ $say = "Hello " . $_GET['name'];
     <h1><?= $say ?></h1>
 </body>
 </html>
+
+<!-- XSS (Cross-site scripting)
+
+http://localhost/belajar-php-web/get.php?name=Cherli%3C/h1%3E%3Cscript%3Ealert(%22Anda%20di%20Hack%22)%3C/script%3E%3Ch1%3EBring%20lagi
+
+-->
