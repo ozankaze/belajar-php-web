3546a2687d8b0b2a10b2adbd1aebd10dcdd06c30
ozankaze <ozankaze101@gmail.com>
2023-05-28 23:08:45 +0700

    Cookie

---
+++ cookie/cookie.php
@@ -0,0 +1,9 @@
+<?php
+
+// 99
+
+setcookie("X-BELAJAR-COOKIE", "Cher Bring");
+
+header("Location: /belajar-php-web/cookie/show-cookie.php");
+
+// Membuat Cookie
---
+++ cookie/show-cookie.php
@@ -0,0 +1,9 @@
+<?php
+
+// 99
+
+$result = $_COOKIE['X-BELAJAR-COOKIE'];
+
+echo $result;
+
+// Mengambil Cookie
